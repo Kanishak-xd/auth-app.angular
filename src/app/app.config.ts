2f963379d5f490b6f17620da58aaf33d5c701c71
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-06-23 16:11:48 +0530

    connected app to firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,21 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// Firebase imports
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+
+    // Routing
+    provideRouter(routes),
+
+    // Firebase
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+  ],
 };
