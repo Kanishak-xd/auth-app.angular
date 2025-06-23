ae910971a461f9a707fe8b00b240016f708d6fde
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-06-23 17:08:15 +0530

    implemented guard for home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,13 @@ import { Login } from './pages/login/login';
 import { Signup } from './pages/signup/signup';
 import { Forgot } from './pages/forgot/forgot';
 
+import { authGuard } from './guards/auth.guard';
+
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: Login },
     { path: 'signup', component: Signup },
     { path: 'forgot-password', component: Forgot },
     { path: 'home', component: Home },
+    { path: 'home', component: Home, canActivate: [authGuard] },
 ];
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { Auth, signOut } from '@angular/fire/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
-  styleUrl: './home.css'
 })
 export class Home {
+  private auth = inject(Auth);
+  private router = inject(Router);
 
+  userEmail: string | null = this.auth.currentUser?.email ?? null;
+  username: string | null = this.auth.currentUser?.displayName ?? null;
+
+  async logout() {
+    await signOut(this.auth);
+    this.router.navigate(['/login']);
+  }
 }
